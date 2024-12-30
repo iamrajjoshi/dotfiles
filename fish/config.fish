9cc1c45f1d7b30cd5d703a06d23ebc4b9fd5961c
Raj Joshi <raj.joshi@sentry.io>
2024-12-30 11:52:02 -0500

    :wrench: chore: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
-# Aliases
 source ~/.config/fish/alias.fish
+
+# Disable the fish greeting message
+set fish_greeting ""
+
+# Set up brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 
 # Starship prompt
 starship init fish | source
